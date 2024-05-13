821ad23342d7adde0d8effadd19e18584821571a
Darren Scott <darrenpipi8@gmail.com>
2024-05-13 13:45:09 +0200

    move towards the direction while punching kinda

--- Assets/Scripts/PunchScripts/PunchScript.cs
+++ Assets/Scripts/PunchScripts/PunchScript.cs
@@ -1,27 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class PunchScript : MonoBehaviour
-{
-    public static bool AllowedToPunch = false; 
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class PunchScript : MonoBehaviour
     {
+        public static bool AllowedToPunch = false;
+        public GameObject PunchPoint;
+        public float radius = 4f;
+        public LayerMask enemyLayer;
+        public LayerMask PillarLayer;
+        public float MovementSpeed = 5f; 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
         
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                AllowedToPunch = true;
+                Shooting.AllowedToShoot = false; 
+            }
+
+            if(Input.GetButtonDown("Fire1") && AllowedToPunch)
+            {
+                Attack();
+              MoveTowardsMousePosition();
+            }
+        }
+
+    private void MoveTowardsMousePosition()
+    {
+        Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseposition.z = 0f;
+
+        Vector3 direction = mouseposition - transform.position;
+        direction.z = 0f;
+        direction.Normalize();
+
+        transform.position += direction * MovementSpeed * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+    private void Attack()
         {
-            AllowedToPunch = true;
-            Shooting.AllowedToShoot = false; 
-         
+            Collider2D[] enemy = Physics2D.OverlapCircleAll(PunchPoint.transform.position, radius, enemyLayer);
+            Collider2D[] pillar = Physics2D.OverlapCircleAll(PunchPoint.transform.position, radius, PillarLayer);
+
+            foreach (Collider2D enemyGameObject in enemy)
+            {
+                Debug.Log("Enemy hit"); 
+            }
+
+            foreach (Collider2D PillarGameObject in pillar)
+            {
+                Debug.Log("Pillar hit");
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(PunchPoint.transform.position, radius);
         }
     }
-}
