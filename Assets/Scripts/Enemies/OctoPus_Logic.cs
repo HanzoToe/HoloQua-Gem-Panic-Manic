c8bcecfb25f387551581873f52b487017d1311a9
HanzoToe <156185889+HanzoToe@users.noreply.github.com>
2024-05-16 00:23:54 +0200

    octopus finish

--- Assets/Scripts/Enemies/OctoPus_Logic.cs
+++ Assets/Scripts/Enemies/OctoPus_Logic.cs
@@ -8,11 +8,11 @@ public class OctoPus_Logic: MonoBehaviour
     private Rigidbody2D rb;
     public GameObject player;
     public GameObject bulletprefab; 
-    public float movespeed = 5f;
+    public float movespeed = 3f;
     public float viewrange = 5f;
-    public float SprayTimer = 3f;
-    public float SprayCooldown = 5f; 
-
+    public float SprayTimer = 0f;
+    bool AllowedToBlast = true;
+    Vector2 moving; 
 
 
     // Start is called before the first frame update
@@ -29,19 +29,58 @@ public class OctoPus_Logic: MonoBehaviour
 
         float distance = Vector2.Distance(player.transform.position, transform.position); 
 
-        if(distance <= viewrange)
+        if(distance <= viewrange && AllowedToBlast)
         {
-            StartBlasting(); 
+            StartCoroutine(StartBlasting()); 
+        }
+
+        if (SprayTimer >= 3f)
+        {
+            StopCoroutine(StartBlasting());
+            AllowedToBlast = false;
+        }
+
+        if(!AllowedToBlast)
+        {
+            SprayTimer -= Time.deltaTime;
+            StartCoroutine(RunAway());
+        }
+
+        if(SprayTimer <= 0f)
+        {
+            AllowedToBlast = true;
+            rb.velocity = Vector3.zero;
+            StopCoroutine(RunAway());
         }
     }
 
-    private void StartBlasting()
+    IEnumerator StartBlasting()
     {
        Instantiate(bulletprefab, gameObject.transform.position, Quaternion.identity);
+        yield return null;
+
+        SprayTimer += Time.deltaTime;
     }
+
+    IEnumerator RunAway()
+    {
+        moving = new Vector2(transform.position.x - player.transform.position.x, transform.position.y - player.transform.position.y);
+        rb.velocity = moving.normalized * movespeed; 
+        yield return null;
+    }
+
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, viewrange);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("PlayerGameObject"))
+        {
+            rb.isKinematic = true;
+            Debug.Log("Player take damage");
+        }
     }
 }
